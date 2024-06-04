f6646395335401ad60f6de9e7b6ec8ef6b0d4173
mohammedumarkhan803@gmail.com <mohammedumarkhan803@gmail.com>
2024-06-04 10:20:44 +0530

    add to cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -3,7 +3,19 @@ import "./Cart.css";
 function Cart({ data }) {
   return (
     <div className="cart">
-      <h1>cart section</h1>
+      <div className="cartList">
+        <div className="cartItem">
+          <img src="images/food1.jpg" alt="img" />
+          <div className="info">
+            <h4>food Item</h4>
+            <span>lhfalhvbaosdhfb</span>
+          </div>
+          <button className="remove-btn">Remove</button>
+        </div>
+      </div>
+      <div className="total-product">
+        <h1>TOTAL PRICE</h1>
+      </div>
     </div>
   );
 }
